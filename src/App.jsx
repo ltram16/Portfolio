4f59e6e75bb8722abaaffdef897c07cbc3f32208
ltram16 <ltram16@gmail.com>
2023-07-02 12:57:25 -0700

    first commit with sample navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+
+const App = () => {
+
+  return (
+    <div>
+    <NavBar />
+    {/* Rest of your portfolio content */}
+  </div>
+  );
+};
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <div className="flex-shrink-0">
+              {/* My logo or brand */}
+              <span className="text-white font-semibold text-xl">Portfolio</span>
+            </div>
+            <div className="hidden md:block">
+              <div className="ml-10 flex items-baseline space-x-4">
+                <a href="#home" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
+                <a href="#about" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
+                <a href="#projects" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
+                <a href="#contact" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
